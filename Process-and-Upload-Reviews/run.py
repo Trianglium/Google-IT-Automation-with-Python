52873f58877de46b8a18c4dd63afa6eaf12e48b5
Jordan Brocker Rudow <70343977+Trianglium@users.noreply.github.com>
2021-11-22 08:19:05 -0600

    Update run.py

--- Process-and-Upload-Reviews/run.py
+++ Process-and-Upload-Reviews/run.py
@@ -2,16 +2,23 @@
 import os
 import requests
 
-url = 'http://'+'[external IP Address]'+'/feedback/'
+# Example External IP Address
+external_IP_Address = 12.345.678.90
 
-fb_path = "/Process-and-Upload-Reviews/feedback/"
-fb_dir = os.listdir(fb_path)
-feedback = {}
-for files in fb_dir:
-    with open(str(fb_path+files), 'r') as fb:
-        fb_sections = fb.read().split('\n')
-        feedback["title"] = fb_sections[0]
-        feedback["name"] = fb_sections[1]
-        feedback["date"] = fb_sections[2]
-        feedback["comment"] = fb_sections[3]
-        post_action = requests.post(url, json=feedback)
+# Enter your website's exdternal IP Address or your django website's URL
+company_url = "http://{}/feedback/".format(External_IP_Address)
+
+# Identify Path of Review (Text) files
+feedback_path = "/Process-and-Upload-Reviews/feedback/"
+# Get the names of all of the Review files that are avaialble in the Directory 
+feedback_dir = os.listdir(feedback_path)
+
+for files in feedback_dir:
+    # Go through each text file, reformat them, and post them to the company's website.
+    with open(feedback_path+files, 'r') as fb:
+        input_sections = fb.read().split('\n')
+        customer_reviews = {"title":input_sections[0], "name":input_sections[1], "date":input_sections[2], "comment":input_sections[3]}
+        post_action = requests.post(company_url, json=customer_reviews)
+        fb.close()
+        
+        
